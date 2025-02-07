c4f62b03a2f926e4cc60f26b173f23842b9167e8
Samael <jalvarado.swe@gmail.com>
2025-02-06 23:53:38 -0800

    Add download resume button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,10 +34,6 @@ export const Hero = () => {
       {/* Content */}
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Created with Next.js
-          </p>
-
           <TextGenerateEffect
             words="Full Responsive Portfolio"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -48,9 +44,9 @@ export const Hero = () => {
             California.
           </p>
 
-          <a href="#about">
+          <a href="/resume.pdf" download>
             <MagicButton
-              title="Show my work"
+              title="Download resume"
               icon={<FaLocationArrow />}
               position="right"
             />
